0bfed0589fe02616f5c1cb5888d2d6672f23e2e1
Antonio Mario Pio <antonio.frioli1@gmail.com>
2023-09-01 23:28:26 -0300

    feat( dataframes support )

--- logistic_regression.jl
+++ logistic_regression.jl
@@ -2,6 +2,7 @@ module LogisticRegression
 
     using Random
     using LinearAlgebra
+    using DataFrames
 
     export predict, initialize_parameters, train
    
@@ -15,13 +16,13 @@ module LogisticRegression
 
     σ(x) = 1 / (1 + exp(-x)) # Sigmoid function
 
-    function predict(X, model)
+    function predict(X::Array, model::LogisticRegressionModel)
         z = (X * model.θ).+ model.bias
         y_pred = σ.(z)
         return y_pred
     end
 
-    function initialize_parameters(num_features, learning_rate)
+    function initialize_parameters(num_features::Int, learning_rate::Float64)
         return LogisticRegressionModel(
             zeros(num_features),
             0.0,
@@ -29,13 +30,28 @@ module LogisticRegression
             )
     end
 
-    function forward(X, θ, b)
+    function forward(X::Matrix, θ::Array, b)
         z = (X * θ).+ b
         y_pred = σ.(z)
         return y_pred
     end
 
-    function train!(X, y, model, num_iterations)
+    function train!(X::Matrix, y::Array, model::LogisticRegressionModel, num_iterations::Int)
+        return backpropagation!(X, y, model, num_iterations)
+    end
+
+    function train!(X::DataFrame, y::Array, model::LogisticRegressionModel, num_iterations::Int)
+        X = Matrix(X)
+        return backpropagation!(X, y, model, num_iterations)
+    end
+
+    function train!(X::DataFrame, y::DataFrame, model::LogisticRegressionModel, num_iterations::Int)
+        X = Matrix(X)
+        y = Matrix(y)
+        return backpropagation!(X, y, model, num_iterations)
+    end
+
+    function backpropagation!(X::Matrix, y::Array, model::LogisticRegressionModel, num_iterations::Int)
         m = length(y)
         for iteration in 1:num_iterations
             y_pred = forward(X, model.θ, model.bias)
